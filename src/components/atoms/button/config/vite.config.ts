17f5718a6bc3ac148709e4cebcd6606027822993
lanceraeltaylor <lancerael@hotmail.com>
2022-09-29 22:20:27 +0100

    Local vite config

---
+++ src/components/atoms/button/config/vite.config.ts
@@ -0,0 +1,25 @@
+/// <reference types="vitest" />
+import { defineConfig } from 'vite'
+import { resolve } from 'node:path'
+
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ["vitest-setup.ts"],
+    coverage: {
+      provider: 'istanbul'
+    },
+  },
+  build: {
+    lib: {
+      entry: resolve(__dirname, '../src/index.ts'),
+      formats: ['es', 'umd'],
+      name: '@pi/button',
+      fileName: 'pi-button',
+    },
+    rollupOptions: {
+      external: ['react', 'styled-components'],
+    }
+  }
+})
