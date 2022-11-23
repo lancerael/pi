f6f9ac2d3ec965ea36ff48ebc7e2c1f1a0d01a8e
lanceraeltaylor <lancerael@hotmail.com>
2022-11-23 16:08:09 +0000

    Cleanup

--- src/components/molecules/AdvancedButton/src/AdvancedButton.tsx
+++ src/components/molecules/AdvancedButton/src/AdvancedButton.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from 'react'
-import { StyledAdvancedButton } from './AdvancedButton.style'
 import Button from '@pi-lib/button'
 import Loader from '@pi-lib/loader'
+import { StyledAdvancedButton } from './AdvancedButton.style'
 import { AdvancedButtonProps } from './AdvancedButton.types'
 
 export const AdvancedButton: FC<AdvancedButtonProps> = ({ isLoading, children, buttonProps } ) => (
