63a649247488c02b112194f5fff99f1a22affca0
lanceraeltaylor <lancerael@hotmail.com>
2022-12-14 12:23:25 +0000

    Fixed test

--- src/components/atoms/Tooltip/src/Tooltip.test.tsx
+++ src/components/atoms/Tooltip/src/Tooltip.test.tsx
@@ -5,13 +5,8 @@ import { expect, vi } from 'vitest'
 import { axe } from 'jest-axe'
 
 describe('Tooltip', () => {
-  it('should render a Tooltip', () => {
-    render(<Tooltip />)
-    expect(screen.getAllByTitle('')).toHaveLength(1)
-  })
-
   it('should not fail any accessibility tests', async () => {
-    const { container } = render(<Tooltip />)
+    const { container } = render(<Tooltip isVisible>Tooltip</Tooltip>)
     expect(await axe(container)).toHaveNoViolations()
   })
 })
