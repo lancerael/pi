c0ab5749cf5b54394239a21227aa90ef2953f1f3
lanceraeltaylor <lancerael@hotmail.com>
2023-03-21 00:19:29 +0000

    Test fixes

--- src/components/atoms/Button/vanilla/index.js
+++ src/components/atoms/Button/vanilla/index.js
@@ -1,15 +1,14 @@
-import { jsx as n } from 'react/jsx-runtime'
-import s, { css as i } from 'styled-components'
-import { box as a } from '@pi-lib/styles'
+import { jsx as n } from "react/jsx-runtime";
+import s, { css as i } from "styled-components";
+import { box as a } from "@pi-lib/styles";
 const c = {
-    default: '#444444',
-    pending: 'blue',
-    error: 'red',
-    success: 'green',
-  },
-  u = s.button(
-    ({ status: o, minWidth: r, margin: t }) => i`
-    ${a({ color: o === 'default' ? '' : c[o] })}
+  default: "#444444",
+  pending: "blue",
+  error: "red",
+  success: "green"
+}, u = s.button(
+  ({ status: o, minWidth: r, margin: t }) => i`
+    ${a({ color: o === "default" ? "" : c[o] })}
     min-width: ${r};
     margin: ${t};
     cursor: pointer;
@@ -22,21 +21,24 @@ const c = {
       cursor: default;
     }
   `
-  ),
-  m = ({
-    children: o,
-    status: r = 'default',
-    dataSelector: t = 'pi-lib-button',
-    minWidth: e = '220px',
-    margin: d = '10px',
-    ...l
-  }) =>
-    /* @__PURE__ */ n(u, {
-      status: r,
-      ...l,
-      minWidth: e,
-      margin: d,
-      'data-selector': t,
-      children: o,
-    })
-export { m as default }
+), m = ({
+  children: o,
+  status: r = "default",
+  dataSelector: t = "pi-lib-button",
+  minWidth: e = "220px",
+  margin: d = "10px",
+  ...l
+}) => /* @__PURE__ */ n(
+  u,
+  {
+    status: r,
+    ...l,
+    minWidth: e,
+    margin: d,
+    "data-selector": t,
+    children: o
+  }
+);
+export {
+  m as default
+};
--- src/config/vanilla/vite.config.js
+++ src/config/vanilla/vite.config.js
@@ -1,41 +1,37 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-export default (libName, customRollup) =>
-  defineConfig({
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+export default (libName, customRollup) => defineConfig({
     test: {
-      globals: true,
-      watch: false,
-      environment: 'jsdom',
-      setupFiles: ['./node_modules/@pi-lib/config/vitest-setup.ts'],
-      coverage: {
-        provider: 'istanbul',
-      },
+        globals: true,
+        watch: false,
+        environment: 'jsdom',
+        setupFiles: ['./node_modules/@pi-lib/config/vitest-setup.ts'],
+        coverage: {
+            provider: 'istanbul',
+        },
     },
     build: {
-      lib: {
-        entry: './src/index.ts',
-        formats: ['es'],
-        name: `@pi-lib/${libName}`,
-        fileName: 'index',
-      },
-      outDir: 'vanilla',
-      rollupOptions:
-        customRollup !== null && customRollup !== void 0
-          ? customRollup
-          : {
-              external: [
+        lib: {
+            entry: './src/index.ts',
+            formats: ['es'],
+            name: `@pi-lib/${libName}`,
+            fileName: 'index',
+        },
+        outDir: 'vanilla',
+        rollupOptions: customRollup !== null && customRollup !== void 0 ? customRollup : {
+            external: [
                 'react',
                 'styled-components',
                 'react/jsx-runtime',
                 '@pi-lib/styles',
-              ],
-              output: {
+            ],
+            output: {
                 globals: {
-                  react: 'React',
+                    react: 'React',
                 },
-              },
             },
+        },
     },
     plugins: [react()],
-  })
+});
