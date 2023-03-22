d3b62018128bfd3caa44cdbcf6bcb71e3bda7cb6
lanceraeltaylor <lancerael@hotmail.com>
2023-03-22 23:05:37 +0000

    Fixing config

---
+++ src/config/CHANGELOG.md
@@ -0,0 +1,61 @@
+# Change Log - @pi-lib/config
+
+This log was last generated on Wed, 15 Mar 2023 22:33:31 GMT and should not be manually modified.
+
+<!-- Start content -->
+
+## 0.2.6
+
+Wed, 15 Mar 2023 22:33:31 GMT
+
+### Patches
+
+- Vanilla dists (lancerael@hotmail.com)
+
+## 0.2.5
+
+Wed, 01 Mar 2023 23:24:27 GMT
+
+### Patches
+
+- [object Object] (lancerael@hotmail.com)
+
+## 0.2.4
+
+Tue, 28 Feb 2023 23:33:14 GMT
+
+### Patches
+
+- [object Object] (lancerael@hotmail.com)
+
+## 0.2.2
+
+Sat, 10 Dec 2022 13:13:39 GMT
+
+### Patches
+
+- Fixed vite config (lancerael@hotmail.com)
+
+## 0.2.1
+
+Tue, 29 Nov 2022 15:26:29 GMT
+
+### Patches
+
+- Cleanup (lancerael@hotmail.com)
+
+## 0.2.0
+
+Mon, 24 Oct 2022 22:49:58 GMT
+
+### Minor changes
+
+- Added descriptions (lancerael@hotmail.com)
+
+## 0.1.0
+
+Sat, 22 Oct 2022 17:32:26 GMT
+
+### Minor changes
+
+- First release (lancerael@hotmail.com)
---
+++ src/config/vite.config.ts
@@ -0,0 +1,47 @@
+/// <reference types="vitest" />
+import { defineConfig, UserConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { RollupOptions } from 'rollup'
+
+import type { InlineConfig } from 'vitest'
+
+interface VitestConfigExport extends UserConfig {
+  test: InlineConfig
+}
+
+export default (libName: string, customRollup?: RollupOptions) => {
+  return defineConfig({
+    test: {
+      globals: true,
+      watch: false,
+      environment: 'jsdom',
+      setupFiles: ['./node_modules/@pi-lib/config/vitest-setup.ts'],
+      coverage: {
+        provider: 'istanbul',
+      },
+    },
+    build: {
+      lib: {
+        entry: './src/index.ts',
+        formats: ['es'],
+        name: `@pi-lib/${libName}`,
+        fileName: 'index',
+      },
+      outDir: 'vanilla',
+      rollupOptions: customRollup ?? {
+        external: [
+          'react',
+          'styled-components',
+          'react/jsx-runtime',
+          '@pi-lib/styles',
+        ],
+        output: {
+          globals: {
+            react: 'React',
+          },
+        },
+      },
+    },
+    plugins: [react()],
+  } as VitestConfigExport)
+}
