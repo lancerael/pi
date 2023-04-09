c4adc815fd036c765ecba2573936b87f382f412d
lanceraeltaylor <lancerael@hotmail.com>
2023-04-09 16:50:04 +0100

    Code cleanup

--- src/components/atoms/Link/src/Link.test.tsx
+++ src/components/atoms/Link/src/Link.test.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import Link from './Link'
 import { render, screen } from '@testing-library/react'
 import { expect, vi } from 'vitest'
--- src/components/atoms/Link/src/Link.tsx
+++ src/components/atoms/Link/src/Link.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react'
+import { FC } from 'react'
 import { StyledLink } from './Link.style'
 import { LinkProps } from './Link.types'
 
