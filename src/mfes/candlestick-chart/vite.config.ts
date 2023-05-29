6f6cc476ad99d38ac4bb9e738f7a27b220cc2f4a
lancerael-undo <122271901+lancerael-undo@users.noreply.github.com>
2023-05-29 15:21:54 +0100

    Build fix

--- src/mfes/candlestick-chart/vite.config.ts
+++ src/mfes/candlestick-chart/vite.config.ts
@@ -14,10 +14,4 @@ export default defineConfig({
       shared: ['react', 'react-dom'],
     }),
   ],
-  build: {
-    modulePreload: false,
-    target: 'esnext',
-    minify: false,
-    cssCodeSplit: false,
-  },
 })
