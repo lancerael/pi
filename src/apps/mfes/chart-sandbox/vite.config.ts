26886af86f9634f1c322215a304485474a0f9651
lancerael <lancerael@googlemail.com>
2023-08-22 23:52:48 +0100

    Fix all cloudfront paths

--- src/apps/mfes/chart-sandbox/vite.config.ts
+++ src/apps/mfes/chart-sandbox/vite.config.ts
@@ -1,39 +1,36 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import federation from '@originjs/vite-plugin-federation'
 import replace from '@rollup/plugin-replace'
 
-export default defineConfig(({ mode }) => {
-  process.env = { ...process.env, ...loadEnv(mode, process.cwd(), '') }
-  return {
-    plugins: [
-      react(),
-      federation({
-        name: 'remote_app',
-        filename: 'remoteSandbox.js',
-        exposes: {
-          './Sandbox': './src/components/organisms/Sandbox/Sandbox',
-          './chartConfigReducer': './src/state/reducers/chartConfigReducer',
-          './chartDataReducer': './src/state/reducers/chartDataReducer',
-        },
-        shared: [
-          'react',
-          'react-dom',
-          '@reduxjs/toolkit',
-          'react-redux',
-          'redux',
-        ],
-      }),
-    ],
-    build: {
-      target: 'esnext',
-      rollupOptions: {
-        plugins: [
-          replace({
-            ___CLOUDFRONT_URL___: process.env.CLOUDFRONT_URL,
-          }),
-        ],
+export default defineConfig({
+  plugins: [
+    react(),
+    federation({
+      name: 'remote_app',
+      filename: 'remoteSandbox.js',
+      exposes: {
+        './Sandbox': './src/components/organisms/Sandbox/Sandbox',
+        './chartConfigReducer': './src/state/reducers/chartConfigReducer',
+        './chartDataReducer': './src/state/reducers/chartDataReducer',
       },
+      shared: [
+        'react',
+        'react-dom',
+        '@reduxjs/toolkit',
+        'react-redux',
+        'redux',
+      ],
+    }),
+  ],
+  build: {
+    target: 'esnext',
+    rollupOptions: {
+      plugins: [
+        replace({
+          ___CLOUDFRONT_URL___: process.env.CLOUDFRONT_URL,
+        }),
+      ],
     },
-  }
+  },
 })
--- src/apps/spas/tech-demo/vite.config.ts
+++ src/apps/spas/tech-demo/vite.config.ts
@@ -1,6 +1,7 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import federation from '@originjs/vite-plugin-federation'
+import replace from '@rollup/plugin-replace'
 
 const portMap = {
   'chart-sandbox': 5001,
@@ -11,43 +12,44 @@ const IS_DEV = process.env.NODE_ENV === 'development'
 const ASSET_PATH = process.env.MFE_ASSET_PATH
 
 const getPath = (id) => {
-  const devUrl = `http://192.168.1.88:${portMap[id]}`
+  const devUrl = `http://localhost:${portMap[id]}`
   if (IS_DEV) return devUrl
   if (ASSET_PATH) return `${ASSET_PATH}/${id}`
   return devUrl
 }
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
-  return {
-    cacheDir: 'node_modules/.cacheDir',
-    plugins: [
-      react(),
-      federation({
-        name: 'app',
-        remotes: {
-          remoteSandbox: `${getPath('chart-sandbox')}/assets/remoteSandbox.js`,
-          remoteCandlestick: `${getPath(
-            'candlestick-chart'
-          )}/assets/remoteCandlestick.js`,
-        },
-        shared: [
-          'react',
-          'react-dom',
-          '@reduxjs/toolkit',
-          'react-redux',
-          'redux',
-        ],
-      }),
-    ],
-    build: {
-      modulePreload: false,
-      target: 'esnext',
-      minify: false,
-      cssCodeSplit: false,
+export default defineConfig({
+  cacheDir: 'node_modules/.cacheDir',
+  plugins: [
+    react(),
+    federation({
+      name: 'app',
+      remotes: {
+        remoteSandbox: `${getPath('chart-sandbox')}/assets/remoteSandbox.js`,
+        remoteCandlestick: `${getPath(
+          'candlestick-chart'
+        )}/assets/remoteCandlestick.js`,
+      },
+      shared: [
+        'react',
+        'react-dom',
+        '@reduxjs/toolkit',
+        'react-redux',
+        'redux',
+      ],
+    }),
+  ],
+  build: {
+    modulePreload: false,
+    target: 'esnext',
+    minify: false,
+    cssCodeSplit: false,
+    rollupOptions: {
+      plugins: [
+        replace({
+          ___CLOUDFRONT_URL___: process.env.CLOUDFRONT_URL,
+        }),
+      ],
     },
-    define: {
-      'process.env.CLOUDFRONT_URL': env.CLOUDFRONT_URL,
-    },
-  }
+  },
 })
