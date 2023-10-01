5d65f0dbc68f45ece17c2d8339444a4c976c0841
lancerael <lancerael@googlemail.com>
2023-10-01 19:31:57 +0100

    Fix props in generator

--- generator/src/_NAME_.tsx
+++ generator/src/_NAME_.tsx
@@ -1,9 +1,10 @@
 import { Styled_NAME_ } from './_NAME_.style'
+import { _NAME_Props } from './_NAME_.types'
 
 /**
  *
  * @returns
  */
-export const _NAME_ = () => <Styled_NAME_ />
+export const _NAME_ = (props: _NAME_Props) => <Styled_NAME_ />
 
 export default _NAME_
