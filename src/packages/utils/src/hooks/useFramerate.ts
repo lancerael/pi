cbacf5502aacb9af10576347b9e9bcbaaae0fd51
lancerael <lancerael@googlemail.com>
2023-10-29 23:40:12 +0000

    Adding starburst and performance adjustments let by new framerate hook

---
+++ src/packages/utils/src/hooks/useFramerate.ts
@@ -0,0 +1,29 @@
+import { useEffect, useRef } from 'react'
+
+export const useFramerate = () => {
+  const counter = useRef(0)
+  const lastTime = useRef(null)
+  const fps = useRef(0)
+  const frame = useRef(0)
+
+  useEffect(() => {
+    const checkFrame = (time) => {
+      counter.current++
+
+      if (counter.current >= 30) {
+        fps.current = lastTime.current
+          ? (counter.current / (time - lastTime.current)) * 1000
+          : 0
+        counter.current = 0
+        lastTime.current = time
+      }
+      frame.current = requestAnimationFrame(checkFrame)
+    }
+
+    frame.current = requestAnimationFrame(checkFrame)
+
+    return () => cancelAnimationFrame(frame.current)
+  }, [])
+
+  return fps
+}
