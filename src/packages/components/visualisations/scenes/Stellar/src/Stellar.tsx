d98d38cb018e26fea3603d5790973cdc78dea901
lancerael <lancerael@googlemail.com>
2023-11-02 14:41:23 +0000

    softer star scrolling

--- src/packages/components/visualisations/scenes/Stellar/src/Stellar.tsx
+++ src/packages/components/visualisations/scenes/Stellar/src/Stellar.tsx
@@ -34,7 +34,7 @@ export const Stellar = ({ starCount = 100, children }: StellarProps) => {
       const newStars = stars.map(
         ({ coords: [left, top], age, ...star }: Star) => {
           return {
-            coords: [left, top - offset * age] as Coords,
+            coords: [left, top - offset * (age / 2)] as Coords,
             age,
             ...star,
           }
