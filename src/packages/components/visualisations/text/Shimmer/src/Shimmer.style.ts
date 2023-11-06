a8c99ff86c148c4cb61d092af8394f52fbcd6628
lancerael <lancerael@googlemail.com>
2023-11-06 00:43:23 +0000

    Add shimmer effect

--- src/packages/components/visualisations/text/Shimmer/src/Shimmer.style.ts
+++ src/packages/components/visualisations/text/Shimmer/src/Shimmer.style.ts
@@ -1,18 +1,35 @@
-import styled, { css } from 'styled-components'
+import styled, { css, keyframes } from 'styled-components'
 import { StyledLineProps } from './Shimmer.style.types'
+
+const pulse = keyframes`
+    0% {
+      margin-left: 0;
+      filter: blur(8px);
+      color: pink;
+    }
+    50% {
+      margin-left: 5px;
+      filter: blur(4px);
+      color: antiquewhite;
+    }
+    100% {
+      margin-left: 0;
+      filter: blur(8px);
+      color: pink;
+    }
+`
 
 export const StyledShimmer = styled.div``
 
 export const StyledLine = styled.div<StyledLineProps>(
   ({ isVisible }) => css`
     font-size: 2rem;
-    opacity: ${isVisible ? 1 : 0};
     height: ${isVisible ? '2rem' : '0'};
-    transform: scaleY(${isVisible ? '100%' : '0%'});
 
     &::after {
-      transition: all 1s linear;
+      transition: all 1.5s linear;
       content: attr(data-content);
+      opacity: ${isVisible ? 1 : 0};
       filter: blur(${isVisible ? '0' : '20px'});
       background-image: linear-gradient(to right, #add8e6, #ffffff);
       background-clip: text;
@@ -22,10 +39,8 @@ export const StyledLine = styled.div<StyledLineProps>(
     &::before {
       transition: all 0.5s ease-in;
       content: attr(data-content);
-      filter: blur(5px);
-      border: 1px solid green;
-      opacity: ${isVisible ? 0.6 : 0};
-      color: pink;
+      animation: ${pulse} 2s linear infinite;
+      opacity: ${isVisible ? 0.7 : 0};
       position: absolute;
     }
   `
