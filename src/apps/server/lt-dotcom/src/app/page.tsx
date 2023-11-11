fcb355be19b43398040a5836f8430d98c2da4017
DESKTOP-7FMBG3E\bugsquish <lancerael@hotmail.com>
2023-11-11 10:54:04 +0000

    Homepage enhancements

--- src/apps/server/lt-dotcom/src/app/page.tsx
+++ src/apps/server/lt-dotcom/src/app/page.tsx
@@ -1,16 +1,16 @@
 'use client'
+import { CSSProperties, useRef, useState } from 'react'
+import styled from 'styled-components'
+import { gradient } from '@pi-lib/styles'
 import Stellar from '@pi-lib/stellar'
 import Grid from '@pi-lib/grid'
-import Interact from '@/components/Interact'
 import Shimmer from '@pi-lib/shimmer'
+import PageGrid from '@pi-lib/page-grid'
+import Banner from '@pi-lib/banner'
 import Card from '@pi-lib/card'
-import { CSSProperties, useRef, useState } from 'react'
 import { tickerLines } from '@/data/tickerLines'
 import { careerHighlights } from '@/data/careerHighlights'
-import PageGrid from '@pi-lib/page-grid'
-import { gradient } from '@pi-lib/styles'
-import styled from 'styled-components'
-import Banner from '@pi-lib/banner'
+import Interact from '@/components/Interact'
 import { skillset } from '@/data/skillset'
 
 const StyledFullGradient = styled.div`
@@ -24,11 +24,12 @@ export default function Home() {
   const [scrollTop, setScrollTop] = useState(0)
   const wrapperRef = useRef<HTMLDivElement>(null)
 
+  const scrollGap = 128
+  const isShimmerAtLimit =
+    wrapperRef.current &&
+    scrollTop > (wrapperRef.current?.offsetHeight ?? 0) / 2 - scrollGap
+
   const getShimmerWrapperStyle = (): CSSProperties => {
-    const scrollGap = 128
-    const isShimmerAtLimit =
-      wrapperRef.current &&
-      scrollTop > (wrapperRef.current?.offsetHeight ?? 0) / 2 - scrollGap
     return {
       position: isShimmerAtLimit ? 'absolute' : 'fixed',
       top: isShimmerAtLimit ? `calc(100vh - ${scrollGap + 18}px)` : undefined,
@@ -42,30 +43,38 @@ export default function Home() {
         <div
           style={{
             position: 'fixed',
-            width: '100%',
+            width: 'calc(100% - 18px)',
             fontSize: '1.4rem',
             fontWeight: 'bold',
             margin: '1rem 0',
-            filter: 'blur(0.5px)',
+            top: '-1rem',
+            opacity: isShimmerAtLimit ? 1 : 0,
           }}
         >
           <PageGrid>
-            <Banner $useBackground={false}>
-              <div
-                style={{
-                  color: 'var(--special)',
-                }}
-              >
-                Lance Taylor
-              </div>
-              <div
-                style={{
-                  color: 'var(--special)',
-                }}
-              >
-                ▶ ⏩ ⏪ 🔽 ⏸
-              </div>
-            </Banner>
+            <div
+              style={{
+                background: 'rgba( 0, 0, 0, 0 )',
+                backdropFilter: 'blur(4px)',
+              }}
+            >
+              <Banner $useBackground={false}>
+                <div
+                  style={{
+                    color: 'var(--special)',
+                  }}
+                >
+                  Lance Taylor
+                </div>
+                <div
+                  style={{
+                    color: 'var(--special)',
+                  }}
+                >
+                  ▶ ⏩ ⏪ 🔽 ⏸
+                </div>
+              </Banner>
+            </div>
           </PageGrid>
         </div>
         <PageGrid>
@@ -110,6 +119,13 @@ export default function Home() {
             <div>Something 3</div>
           </StyledFullGradient>
           <div style={{ margin: '1rem' }}>
+            <div style={{ textAlign: 'center', marginBottom: '2rem' }}>
+              <Shimmer
+                lines={['Career Highlights']}
+                behaviour="linger"
+                delay={0}
+              />
+            </div>
             <Grid>
               {careerHighlights.map(({ title, subTitle, summary, bullets }) => (
                 <Card key={title} {...{ title, subTitle }}>
@@ -125,6 +141,14 @@ export default function Home() {
               ))}
             </Grid>
           </div>
+
+          <StyledFullGradient className="pi-page-grid-full">
+            <Grid>
+              <div>Something 1</div>
+              <div>Something 2</div>
+              <div>Something 3</div>
+            </Grid>
+          </StyledFullGradient>
         </PageGrid>
       </Stellar>
     </div>
