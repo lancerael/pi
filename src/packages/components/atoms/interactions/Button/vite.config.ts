b59d4ea680a58637e4a1978c340453af276406f4
lancerael <lancerael@googlemail.com>
2023-12-04 00:25:24 +0000

    Code cleanup

--- src/packages/components/atoms/interactions/Button/vite.config.ts
+++ src/packages/components/atoms/interactions/Button/vite.config.ts
@@ -1,16 +1,3 @@
 import createConfig from '@pi-lib/config'
-import resolve from 'rollup-plugin-node-resolve'
-import typescript from 'rollup-plugin-typescript2'
-import keysTransformer from 'ts-transformer-keys/transformer'
 
-export default createConfig('button', undefined, [
-  resolve(),
-  typescript({
-    transformers: [
-      (service) => ({
-        before: [keysTransformer(service.getProgram()!)],
-        after: [],
-      }),
-    ],
-  }),
-])
+export default createConfig('button')
